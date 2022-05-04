77d86e2e69ca156d19597de7fb3a06161630bdab
uncharted220501@hotmail.com <uncharted220501@hotmail.com>
2022-05-04 21:47:55 +0200

    oploading project

--- app/src/main/java/dk/hovdeforlob4/android/thetennisball/MainActivity.kt
+++ app/src/main/java/dk/hovdeforlob4/android/thetennisball/MainActivity.kt
@@ -1,5 +1,6 @@
 package dk.hovdeforlob4.android.thetennisball
 
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -40,11 +41,43 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
-        TODO("Not yet implemented")
+    override fun onSensorChanged(event: SensorEvent?) {
+        val ball = findViewById<ImageView>(R.id.imageView_move)
+
+        if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            val sides = event.values[0]
+            val upDown = event.values[1]
+
+//            square.apply {
+//                rotationX = upDown * 3f
+//                rotationY = sides * 3f
+//                rotation = -sides
+//                translationX = sides * -10
+//                translationY = upDown * 10
+//            }
+
+            ball.apply {
+                x = sides * 50f
+                y = upDown * 50f
+
+                translationX = sides * -50
+                translationY = upDown * 150
+            }
+
+
+            val colour = if (upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.RED
+            square.setBackgroundColor(colour)
+
+            square.text = "up/down ${upDown.toInt()}\nleft/right ${sides.toInt()}"
+        }
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 }
